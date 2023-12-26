e21464c916a522b3f7938a7f678ea06560a5eef1
flying <1290830173@qq.com>
2023-12-26 17:32:55 +0800

    visualize_sensor.py

--- visualize_sensor.py
+++ visualize_sensor.py
@@ -2,23 +2,26 @@
 import pandas as pd
 import folium
 
-filepath = 'libcity/cache/graph_sensor_locations.csv'
-df = pd.read_csv(filepath)
+for dataset in ['metr_la', 'pems_bay']:
+    if dataset == 'metr_la':
+        filepath = f'libcity/cache/graph_sensor_locations.csv'
+        df = pd.read_csv(filepath)
+    else:
+        filepath = 'libcity/cache/graph_sensor_locations_bay.csv'
+        df = pd.read_csv(filepath, names=['sensor_id', 'latitude', 'longitude'])
+        df.insert(0, 'index', len(df), allow_duplicates=False)
 
-# filepath = 'libcity/cache/graph_sensor_locations_bay.csv'
-# df = pd.read_csv(filepath, names=['sensor_id', 'latitude', 'longitude'])
+    mean_latitude = df['latitude'].mean()
+    mean_longitude = df['longitude'].mean()
 
-mean_latitude = df['latitude'].mean()
-mean_longitude = df['longitude'].mean()
+    m = folium.Map(location=(mean_latitude, mean_longitude), zoom_start=12)
 
-m = folium.Map(location=(mean_latitude, mean_longitude), zoom_start=12)
+    for data in df.iterrows():
+        tmp_index = data[1]['index']
+        tmp_latitude = data[1]['latitude']
+        tmp_longitude = data[1]['longitude']
+        tmp_sensor_id = data[1]['sensor_id']
+        folium.Marker(location=(tmp_latitude, tmp_longitude),
+                      popup=f'({int(tmp_index)},{int(tmp_sensor_id)}):({tmp_latitude},{tmp_longitude})').add_to(m)
 
-for data in df.iterrows():
-    tmp_latitude = data[1]['latitude']
-    tmp_longitude = data[1]['longitude']
-    tmp_sensor_id = data[1]['sensor_id']
-    folium.Marker(location=(tmp_latitude, tmp_longitude),
-                  popup='{}:({},{})'.format(int(tmp_sensor_id), tmp_latitude, tmp_longitude)).add_to(m)
-
-m.save('map_metr_la.html')
-# m.save('map_pems_bay.html')
+    m.save(f'map_{dataset}.html')
