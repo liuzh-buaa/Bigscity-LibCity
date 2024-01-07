49d2f84939e37ec329378b6c2a7599709d5c3f83
flyingrabbit <1290830173@qq.com>
2024-01-07 22:59:38 +0800

    analyse

--- analyse_testing_result4.py
+++ analyse_testing_result4.py
@@ -1,5 +1,9 @@
 """
     根据某个传感器某个时刻的速度绘制此时受其他传感器的影响
+    `index`: 对应某个时刻
+    `nn`: 对应某个传感器
+    `output_window`: [2,5,11]，根据输出的哪个预测窗口算平均
+    `input_dim`: [0,1,-1]，对哪个输入维度做检验，如果是-1则取两个维度平均值
 """
 import argparse
 import os.path
@@ -16,9 +20,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--dataset', type=str, default='METR_LA', choices=['METR_LA', 'PEMS_BAY'])
     parser.add_argument('--index', type=int, default=19)
-    parser.add_argument('--ow', type=int, default=2, help='output window')
     parser.add_argument('--nn', type=int, default=0, help='node number')
     parser.add_argument('--od', type=int, default=0, help='output dim')
+    parser.add_argument('--output_window', type=int, default=2, choices=[2, 5, 11])
+    parser.add_argument('--input_dim', type=int, default=0, choices=[0, 1, -1])
 
     # 解析参数
     args = parser.parse_args()
@@ -26,7 +31,7 @@ if __name__ == '__main__':
     sensors = 207 if args.dataset == 'METR_LA' else 325
     exp_id = get_exp_id(args.dataset, args.index)
 
-    res_dir = f'analyse_testing_result4/{args.dataset}/{args.nn}'
+    res_dir = f'analyse_testing_result4/{args.dataset}/{args.nn}/{args.output_window}_{args.input_dim}'
     ensure_dir(res_dir)
 
     testing_res_dir = './libcity/cache/{}/testing_cache'.format(exp_id)
@@ -34,12 +39,18 @@ if __name__ == '__main__':
 
     ext = {}
     for test_nn in range(sensors):
-        filename = 'ps_testing_{}_{}_{}_{}_{}.npy'.format(args.index, 2, args.nn, args.od, test_nn)
+        filename = 'ps_testing_{}_{}_{}_{}_{}.npy'.format(args.index, args.output_window, args.nn, args.od, test_nn)
         read_data = 1 - np.load(os.path.join(testing_res_dir, filename))
 
         if test_nn == args.nn:
-            ext[test_nn] = read_data[2, 0]
+            if args.input_dim == -1:
+                ext[test_nn] = np.mean(read_data[11, :])
+            else:
+                ext[test_nn] = read_data[11, args.input_dim]
         else:
-            ext[test_nn] = read_data[0, 0]
+            if args.input_dim == -1:
+                ext[test_nn] = np.mean(read_data[0, :])
+            else:
+                ext[test_nn] = read_data[0, args.input_dim]
 
     visualize_sensor_varying('METR_LA', args.nn, filename=f'{res_dir}/{args.nn}_{args.index}.html', ext=ext)
--- analyse_testing_result5.py
+++ analyse_testing_result5.py
@@ -1,5 +1,9 @@
 """
     根据所有预测结果，评估某个传感器对其他传感器的影响平均值
+    `indices`: 所有的预测结果范围
+    `include_self`: True/False，计算影响时是否包含对自己的影响
+    `output_window`: [2,5,11]，根据输出的哪个预测窗口算平均
+    `input_dim`: [0,1,-1]，对哪个输入维度做检验，如果是-1则取两个维度平均值
 """
 import argparse
 import os.path
@@ -8,28 +12,28 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from analyse_testing_result import get_exp_id
-from libcity.utils import ensure_dir
+from libcity.utils import ensure_dir, str2bool
 from visualize_sensor import visualize_sensor_varying
 
 if __name__ == '__main__':
     plt.rc('font', family='Times New Roman', size=20)
     parser = argparse.ArgumentParser()
     parser.add_argument('--dataset', type=str, default='METR_LA', choices=['METR_LA', 'PEMS_BAY'])
-    parser.add_argument('--ow', type=int, default=2, help='output window')
-    parser.add_argument('--od', type=int, default=0, help='output dim')
+    parser.add_argument('--include_self', type=str2bool)
+    parser.add_argument('--output_window', type=int, default=2, choices=[2, 5, 11])
+    parser.add_argument('--input_dim', type=int, default=0, choices=[0, 1, -1])
 
     # 解析参数
     args = parser.parse_args()
 
     sensors = 207 if args.dataset == 'METR_LA' else 325
     indices = list(range(0, 25))
-    res_dir = f'analyse_testing_result5/{args.dataset}'
+    res_dir = f'analyse_testing_result5/{args.dataset}/{args.include_self}_{args.output_window}_{args.input_dim}'
     ensure_dir(res_dir)
 
     ll = []
     for index in indices:
         ext_filename = f'{res_dir}/ext_{index}.npy'
-        # ext_filename = f'{res_dir}/ext_{index}_total.npy'
         if os.path.isfile(ext_filename):
             ext = np.load(ext_filename, allow_pickle=True).item()
             print(f'Loading from {ext_filename}.')
@@ -40,30 +44,33 @@ if __name__ == '__main__':
             assert os.path.exists(testing_res_dir)
             for nn in range(sensors):
                 for test_nn in range(sensors):
-                    if test_nn == nn:
+                    if not args.include_self and test_nn == nn:
                         continue
 
-                    filename = 'ps_testing_{}_{}_{}_{}_{}.npy'.format(index, 2, nn, args.od, test_nn)
-                    read_data = np.load(os.path.join(testing_res_dir, filename))
+                    filename = 'ps_testing_{}_{}_{}_{}_{}.npy'.format(index, args.output_window, nn, 0, test_nn)
+                    read_data = 1 - np.load(os.path.join(testing_res_dir, filename))
 
                     if test_nn == nn:
-                        ext[test_nn] += read_data[2, 0]
+                        if args.input_dim == -1:
+                            ext[test_nn] += np.mean(read_data[11, :])
+                        else:
+                            ext[test_nn] += read_data[11, args.input_dim]
                     else:
-                        ext[test_nn] += read_data[0, 0]
+                        if args.input_dim == -1:
+                            ext[test_nn] += np.mean(read_data[0, :])
+                        else:
+                            ext[test_nn] += read_data[0, args.input_dim]
             np.save(f'{ext_filename}', ext)
             print(f'Saving ext to {ext_filename}.')
         ll.append(ext)
         visualize_sensor_varying('METR_LA', -1, filename=f'{res_dir}/significance_{index}.html', ext=ext, adjust=True)
-        # visualize_sensor_varying('METR_LA', -1, filename=f'{res_dir}/significance_{index}_total.html', ext=ext, adjust=True)
         print(f'Finish visualize index {index}.')
 
     ext_filename = f'{res_dir}/ext.npy'
-    # ext_filename = f'{res_dir}/ext_total.npy'
     if os.path.isfile(ext_filename):
         ext = np.load(ext_filename, allow_pickle=True).item()
     else:
         ext = {sensor: sum([ext_l[sensor] for ext_l in ll]) for sensor in range(sensors)}
         np.save(f'{ext_filename}', ext)
     visualize_sensor_varying('METR_LA', -1, filename=f'{res_dir}/significance.html', ext=ext, adjust=True)
-    # visualize_sensor_varying('METR_LA', -1, filename=f'{res_dir}/significance_total.html', ext=ext, adjust=True)
     print(f'Finish visualize.')
